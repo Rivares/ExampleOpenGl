62481c3c879f64ab79948f15720b5858ea0dac00
Rivares <ura-nikolaevic@yandex.ru>
2016-03-13 22:48:05 +0300

    Clean store. Add switch and output Tjyd

--- ExampleOpenGl/ExampleOpenGl.cpp
+++ ExampleOpenGl/ExampleOpenGl.cpp
@@ -9,8 +9,6 @@
 #include <thread>
 #include <cstdarg>
 #include <stdio.h>
-#include <gl\gl.h>
-#include <gl\glu.h>
 #include <time.h>
  
 using namespace std;
@@ -37,11 +35,12 @@ void output(GLfloat x, GLfloat y, char* text)
 }
 
 
-void displayMeTpar()
+void displayMe()
 {
-	double cntrlpointsP[10000][5];
+	double grphpnts[10000][5];
 	unsigned int N = 10000;
 	short z = 5;
+	char md = '0';
 	double  Rp = 0.0191806, Rj = 0.0000777, t1 = 3, Gamma1 = (0.05654433 * t1) / 0.5, Gamma2 = (0.0002291314 * t1) / 0.5,
 		    buf1[5] = {160, 160,  156, 151.99, 147.99}, buf2[5] = {120.37, 120.37, 124.38, 128.38, 132.39};
 
@@ -103,24 +102,57 @@ void displayMeTpar()
        }
     }
 
+	//------------------------------------------------------------------------------------------------
+	// Switch need function:
+	float cP = 4.358974358, kP = 0.66666, cG = 5.132075471, cDT = 0;
+	char *T0 = "0", *T1 = "0", *T2 = "0";
+	cout << "Switch need function! P or G: ";
+	cin >> md;
 
-    for(unsigned int i = 0; i < N; i++) // 1(0)
-    {
-       for(short j = 0; j < z; j++)
-       {
-		   cntrlpointsP[i][j] = x1[i][j];
-		   //cntrlpointsJ[i][j] = y1[i][j];
-       }
-       cout << endl;
-    }
-	
+	if (md == 'P')
+	{
+		for(unsigned int i = 0; i < N; i++) // 1(0)
+		{
+		  for(short j = 0; j < z; j++)
+		   {
+			   grphpnts[i][j] = cP * x1[i][j];
+		   }
+		   cout << endl;
+		}
+		cDT = 1;
+		T0 = "160", T1 = "156", T2 = "151.99";
+	}
+	else
+	{
+	  if (md == 'G')
+	  {
+		for(unsigned int i = 0; i < N; i++) // 1(0)
+		{
+		  for(short j = 0; j < z; j++)
+		   {
+			   grphpnts[i][j] = cG * y1[i][j];
+		   }
+		   cout << endl;
+		}
+		cDT = 0.1;
+		T0 = "128.38", T1 = "124.38", T2 = "120.37";
+	  }
+	  else
+	  {
+		  cout << "Error input!!!" << endl;
+		  return;
+	  }
+	}
+
+
 	//------------------------------------------------------------------------------------------------
 	
     glClear(GL_COLOR_BUFFER_BIT);
     glBegin(GL_LINES);
 
+		// Drawing axis coordinates:
 	    short x0 = 25, xN = 1000, y0 = 5, yN = 680;
-	    // Drawing axis coordinates:
+	    
         glVertex2f(x0, y0);                          // one point of coordinates axis x
         glVertex2f(xN, y0);
 
@@ -137,20 +169,21 @@ void displayMeTpar()
 		glVertex2f(x0, yN);							// two part point of axis y
         glVertex2f(x0+4, yN-10);		
 
-	    //  Drawing lines on axis y:     ( x = 3.846153 - coeff scalability;  156 * x = 600 ); 3.846153*0.66666*cntrlpointsP[0][1]
-	    glVertex2f( x0+3, 3.846153*0.66666*cntrlpointsP[0][1] );		// 160
-        glVertex2f( x0-3, 3.846153*0.66666*cntrlpointsP[0][1] );
+	    //  Drawing lines on axis y:     ( x = 5.132075471 - coeff scalability;  156 * x = 680 ); 5.132075471*0.66666*grphpnts[0][1]
 
-		glVertex2f( x0+3, 3.846153*cntrlpointsP[0][2]/2 );			   // 156
-        glVertex2f( x0-3, 3.846153*cntrlpointsP[0][2]/2 );
+	    glVertex2f( x0+3, kP*grphpnts[0][1] );		// T0
+        glVertex2f( x0-3, kP*grphpnts[0][1] );
 
-		glVertex2f( x0+3, 3.846153*cntrlpointsP[0][3]/3 );			   // 151.99
-        glVertex2f( x0-3, 3.846153*cntrlpointsP[0][3]/3 );
+		glVertex2f( x0+3, grphpnts[0][2]/2 );	    // T1
+        glVertex2f( x0-3, grphpnts[0][2]/2 );
+
+		glVertex2f( x0+3, grphpnts[0][3]/3 );		// T2
+        glVertex2f( x0-3, grphpnts[0][3]/3 );
 
 
-		for(unsigned int i = 25; i < N-10; i += 50)
+		for(short i = 25; i < xN; i += 59)
 		{
-		    glVertex2f(i, y0+3);									   // other lines on axis x
+		    glVertex2f(i, y0+3);					 // other lines on axis x
             glVertex2f(i, y0-3);
 		}
 
@@ -159,20 +192,20 @@ void displayMeTpar()
 		for(short i = 1; i < (z-1); ++i)  // 1(1)
 		{
 			double xbuf = 25;
-			for(unsigned int m = 0; m < N-1; ++m)
+			for(unsigned int m = 0; m < N; ++m)
 			{
 				double buf1 = 0;			
-				buf1 = (3.846153) * cntrlpointsP[m][i];
+				buf1 = grphpnts[m][i];
 				if (i == 1)										     // for scalability
-					glVertex2f(xbuf, buf1/(i/0.66666));
+					glVertex2f(xbuf, buf1*kP/i);
 				else 
 					glVertex2f(xbuf, buf1/i);	
 
-				xbuf += 1;									        // step
+				xbuf += cDT;									     // step
 
-				buf1 = (3.846153) * cntrlpointsP[m+1][i];
+				buf1 = grphpnts[m+1][i];
 				if (i == 1)
-					glVertex2f(xbuf, buf1/(i/0.66666));
+					glVertex2f(xbuf, buf1*kP/i);
                 else 
 					glVertex2f(xbuf, buf1/i);
 			}
@@ -183,12 +216,12 @@ void displayMeTpar()
 	glEnd();
 	
 	output(35, 660, "T,'C");  // 2(0)
-	output(860, 50, "t*10^(4) sec");
+	output(880, 50, "t*10^3 sec");
 	output(10, 5, "0");
-
-	output(x0+1, (3.846153*0.66666*cntrlpointsP[0][1])+4 , "160");
-	output(x0+1, (3.846153*cntrlpointsP[0][2]/2)+4 , "156");
-	output(x0+1, (3.846153*cntrlpointsP[0][3]/3)+4 , "151.99");
+	
+	output(x0+1, (kP*grphpnts[0][1])+12 , T0);
+	output(x0+1, (grphpnts[0][2]/2)+12 , T1);
+	output(x0+1, (grphpnts[0][3]/3)+12 , T2);
 	
 /*  union cnvrt
 	{
@@ -201,7 +234,7 @@ void displayMeTpar()
 
 
 	
-	char it[]={"0.4  0.8  1.2  1.6  2   2.4  2.8  3   3.4  3.8  4.2  4.6  5   5.4  5.8  6.2  6.6  7   7.2"};
+	char it[]={"0.625 1.25  1.875 2.5  3.125 3.75 4.375  5  5.625  6.25  6.875 7.5  8.125 8.75 9.375  10"};    // supposably for two functions
 	output(x0+(35), y0+10,  it);
 
 
@@ -235,203 +268,7 @@ void displayMeTpar()
 	*/
 }
 
-void displayMeTjyd()
-{
-	double cntrlpointsP[10000][5];
-	unsigned int N = 10000;
-	short z = 5;
-	double  Rp = 0.0191806, Rj = 0.0000777, t1 = 3, Gamma1 = (0.05654433 * t1) / 0.5, Gamma2 = (0.0002291314 * t1) / 0.5,
-		    buf1[5] = {160, 160,  156, 151.99, 147.99}, buf2[5] = {120.37, 120.37, 124.38, 128.38, 132.39};
 
-    vector<double> bmp(5, 0);
-    vector<vector<double> > x1(N,bmp);
-    vector<vector<double> > y1(N,bmp);
-
-    thread t0(x1Thread, N, ref(x1), buf1);
-    thread t9(y1Thread, N, ref(y1), buf2);
-
-    t0.join();
-    t9.join();
-/*
-	double **x1 = new double *[N];  // 1(1)
-	for(int i = 0; i < N; i++)
-    {
-        x1[i] = new double [5];
-    }
-
-
-	double **y1 = new double *[N];  // 1(2)
-	for(int i = 0; i < N; i++)
-    {
-        y1[i] = new double [5];
-    }
-
-
-    for(int i = 0; i < N; i++)
-    {
-       x1[i][0] = 160;  // 2(1)
-       x1[i][1] = 0; x1[i][2] = 0; x1[i][3] = 0;
-       x1[i][4] = 147.99;
-
-       y1[i][0] = 120.37;  // 2(2)
-       y1[i][1] = 0; y1[i][2] = 0; y1[i][3] = 0;
-       y1[i][4] = 132.39;
-    }
-
-
-    for(int i = 0; i < 5; i++)  // 3(1)
-    {
-       x1[0][i] = buf1[i];
-    }
-
-	for(int i = 0; i < 5; i++) // 3(2)
-    {
-       y1[0][i] = buf2[i];
-    }
-*/
-
-    // DEFINED FOR ALL SCHEMA
-
-    for(unsigned int i = 1; i < N; i++)   // n: t
-    {
-       for(short j = 1; j < (z-1); j++)  // i: z
-       {
-		   x1[i][j] = x1[i-1][j] * (-Gamma1 + 1 - (t1 * Rp)) + (Gamma1 * x1[i-1][j-1]) + (t1 * Rp * y1[i-1][(z-1)-j]);
-		   y1[i][j] = y1[i-1][j] * (-Gamma2 + 1 - (t1 * Rj)) + (Gamma2 * y1[i-1][j-1]) + (t1 * Rj * x1[i-1][(z-1)-j]);   	   
-       }
-    }
-
-
-    for(unsigned int i = 0; i < N; i++)
-    {
-       for(short j = 0; j < z; j++)
-       {
-		   cntrlpointsP[i][j] = x1[i][j];
-		   //cntrlpointsJ[i][j] = y1[i][j];
-       }
-       cout << endl;
-    }
-	
-	//------------------------------------------------------------------------------------------------
-	
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_LINES);
-
-	    short x0 = 25, xN = 1000, y0 = 5, yN = 680;
-	    // Drawing axis coordinates:
-        glVertex2f(x0, y0);                          // one point of coordinates axis x
-        glVertex2f(xN, y0);
-
-		glVertex2f(xN, y0);							// one part point of axis x
-        glVertex2f(xN-10, y0+5);
-		glVertex2f(xN, y0);							// two part point of axis x
-        glVertex2f(xN-10, y0-5);
-
-        glVertex2f(x0, y0);							// one point of coordinates axis y
-	    glVertex2f(x0, yN);
-    
-		glVertex2f(x0, yN);							// one part point of axis y
-        glVertex2f(x0-4, yN-10);
-		glVertex2f(x0, yN);							// two part point of axis y
-        glVertex2f(x0+4, yN-10);		
-
-	    //  Drawing lines on axis y:     ( x = 3.846153 - coeff scalability;  156 * x = 600 ); 3.846153*0.66666*cntrlpointsP[0][1]
-	    glVertex2f( x0+3, 3.846153*0.66666*cntrlpointsP[0][1] );		// 160
-        glVertex2f( x0-3, 3.846153*0.66666*cntrlpointsP[0][1] );
-
-		glVertex2f( x0+3, 3.846153*cntrlpointsP[0][2]/2 );			   // 156
-        glVertex2f( x0-3, 3.846153*cntrlpointsP[0][2]/2 );
-
-		glVertex2f( x0+3, 3.846153*cntrlpointsP[0][3]/3 );			   // 151.99
-        glVertex2f( x0-3, 3.846153*cntrlpointsP[0][3]/3 );
-
-
-		for(unsigned int i = 25; i < N-10; i += 50)
-		{
-		    glVertex2f(i, y0+3);									   // other lines on axis x
-            glVertex2f(i, y0-3);
-		}
-
-				
-	    //  Drawing function to lines:
-		for(short i = 1; i < (z-1); ++i)
-		{
-			double xbuf = 25;
-			for(unsigned int m = 0; m < N-1; ++m)
-			{
-				double buf1 = 0;			
-				buf1 = (3.846153) * cntrlpointsP[m][i];
-				if (i == 1)										     // for scalability
-					glVertex2f(xbuf, buf1/(i/0.66666));
-				else 
-					glVertex2f(xbuf, buf1/i);	
-
-				xbuf += 1;									        // step
-
-				buf1 = (3.846153) * cntrlpointsP[m+1][i];
-				if (i == 1)
-					glVertex2f(xbuf, buf1/(i/0.66666));
-                else 
-					glVertex2f(xbuf, buf1/i);
-			}
-	    }
-		
-	
-		
-	glEnd();
-	
-	output(35, 660, "T,'C");
-	output(860, 50, "t*10^(4) sec");
-	output(10, 5, "0");
-
-	output(x0+1, (3.846153*0.66666*cntrlpointsP[0][1])+4 , "160");
-	output(x0+1, (3.846153*cntrlpointsP[0][2]/2)+4 , "156");
-	output(x0+1, (3.846153*cntrlpointsP[0][3]/3)+4 , "151.99");
-	
-/*  union cnvrt
-	{
-	    float dt;
-		char bdt[sizeof(float)];
-	};
-
-    cnvrt it;
-*/
-
-
-	
-	char it[]={"0.4  0.8  1.2  1.6  2   2.4  2.8  3   3.4  3.8  4.2  4.6  5   5.4  5.8  6.2  6.6  7   7.2"};
-	output(x0+(35), y0+10,  it);
-
-
-
-    glFlush();
-
-		
-	cout << "Difference time: " << (clock() - timeBeg) / 1000 << endl;
-	
-/*
-	
-    for(int i = 0; i < 10; i++)
-    {
-       for(int j = 0; j < 5; j++)
-       {
-		   cout << x1[i][j] << " / ";
-       }
-       cout << endl;
-    }
-
-	cout << endl << endl;
-
-	for(int i = 0; i < 10000; i++)
-    {
-       for(int j = 0; j < 5; j++)
-       {
-		   cout << y1[i][j] << " / ";
-       }
-       cout << endl;
-    }
-	*/
-}
 
 int main(int argc, char** argv)
 {
@@ -446,13 +283,10 @@ int main(int argc, char** argv)
     glLoadIdentity();
     gluOrtho2D(0, 1000, 0, 680);
 
-	glutDisplayFunc(displayMeTpar);
+	glutDisplayFunc(displayMe);
     glutMainLoop();
 
-	glutDisplayFunc(displayMeTjyd);
-    glutMainLoop();
-	
-	
+		
     return 0;
 }
 
